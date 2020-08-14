9ce9279031b5d4481a4a1f34fe52256216418c0b
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2020-08-13 23:42:22 -0700

    Add User Model -- Uzair

---
+++ MCNMedia/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCNMedia_Dev.Models
+{
+    public class User
+    {
+        public int User_Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
