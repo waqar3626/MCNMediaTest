8598e4d546bba27683e1c1c36e5f6957510d184c
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2020-08-19 03:04:26 -0700

    Add UserRole Class --Khalid--

---
+++ MCNMedia/Models/UserRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCNMedia_Dev.Models
+{
+
+    public class UserRoles
+    {
+        public int RoleId { get; set; }
+        public String RoleName { get; set; }
+    }
+
+}
