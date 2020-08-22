39089a2e0a883e4eaf9cec46227a59b1e00ea2d1
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2020-08-21 23:16:15 -0700

    scheduledataAccess--Khalid--

---
+++ MCNMedia/Models/Schedule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCNMedia_Dev.Models
+{
+    public class Schedule
+    {
+        public int ScheduleId { get; set; }
+        public int ChurchId { get; set; }
+        public string ChurchName { get; set; }
+        [Required]
+        public String EventName { get; set; }
+        //[DisplayFormat(DataFormatString = "{0:yyyy/MMM/dd}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MMM/dd/yyyy}")]
+        public DateTime EventDate { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:ddd}")]
+        public DateTime EventDay { get; set; }
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:hh:mm}")]
+        [DataType(DataType.Time)]
+        public DateTime EventTime { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        //[DataType(DataType.Date)]
+        //[Display(Name = "Required delivery date")]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        //public Nullable<DateTime> DelDt { get; set; }
+
+    }
+}
---
+++ MCNMedia/Models/scheduleDataAccessLayer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCNMedia_Dev.Models
+{
+    public class scheduleDataAccessLayer
+    {
+        AwesomeDal.DatabaseConnect _dc;
+        private string v;
+        public scheduleDataAccessLayer()
+        {
+            _dc = new AwesomeDal.DatabaseConnect();
+        }
+        public scheduleDataAccessLayer(string v)
+        {
+            this.v = v;
+            _dc = new AwesomeDal.DatabaseConnect();
+        }
+
+
+        //To Add new Schedule record    
+        public void AddSchedule(Schedule schedules)
+        {
+            _dc.ClearParameters();
+            _dc.AddParameter("ScheduleEventName", schedules.EventName);
+            _dc.AddParameter("ScheduleEventDate", schedules.EventDate);
+            _dc.AddParameter("ScheduleEventDay", schedules.EventDay);
+            _dc.AddParameter("ScheduleEventTime", schedules.EventDay);
+            _dc.AddParameter("ChurchId", schedules.ChurchId);
+            _dc.AddParameter("CreatedBy", 1);
+            //_dc.AddParameter("RoleId", schedules.RoleId);
+            _dc.Execute("spschedule_Add");
+        }
+
+        //To View all Users details 
+        public IEnumerable<Schedule> GetAllSchedule()
+        {
+            List<Schedule> Balobj = new List<Schedule>();
+            _dc.ClearParameters();
+            _dc.AddParameter("SchId", -1);
+            _dc.AddParameter("SchName", "");
+            _dc.AddParameter("ChrId", -1);
+            //_dc.AddParameter("EmailAdd", "");
+            DataTable dataTable = _dc.ReturnDataTable("spSchedule_Search");
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                Schedule schedule = new Schedule();
+                schedule.ScheduleId = Convert.ToInt32(dataRow["ScheduleId"]);
+                schedule.ChurchName = dataRow["ChurchName"].ToString();
+                schedule.EventName = dataRow["ScheduleEventName"].ToString();
+                schedule.EventDate = Convert.ToDateTime(dataRow["ScheduleEventDate"].ToString());
+                schedule.EventDay = Convert.ToDateTime(dataRow["ScheduleEventDay"].ToString());
+                schedule.EventTime = Convert.ToDateTime(dataRow["ScheduleEventTime"].ToString());
+                schedule.CreatedAt = Convert.ToDateTime(dataRow["CreatedAt"].ToString());
+                //user.UpdatedBy = Convert.ToInt32(rdr["UpdatedBy"]);
+                //schedule.RoleName = dataRow["RoleName"].ToString();
+
+
+                Balobj.Add(schedule);
+            }
+            return Balobj;
+        }
+
+        //Get the details of a particular User
+        public Schedule GetScheduleDataBtId(int id)
+        {
+            Schedule schedule = new Schedule();
+
+            _dc.ClearParameters();
+            _dc.AddParameter("SchId", id);
+            DataTable dataTable = _dc.ReturnDataTable("spSchedule_GetById");
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                schedule.ScheduleId = Convert.ToInt32(dataRow["ScheduleId"]);
+                schedule.ChurchId = Convert.ToInt32(dataRow["ChurchId"]);
+                schedule.EventName = dataRow["ScheduleEventName"].ToString();
+                schedule.EventDate = Convert.ToDateTime(dataRow["ScheduleEventDate"].ToString());
+                schedule.EventDay = Convert.ToDateTime(dataRow["ScheduleEventDay"].ToString());
+                schedule.EventTime = Convert.ToDateTime(dataRow["ScheduleEventTime"].ToString());
+                //user.UpdatedBy = Convert.ToInt32(rdr["UpdatedBy"]);
+                //schedule.RoleId = Convert.ToInt32(dataRow["RoleId"]);
+            }
+            return schedule;
+        }
+
+        //To Update the records of a particluar User
+        public void UpdateSchedule(Schedule schedule)
+        {
+            _dc.ClearParameters();
+            _dc.AddParameter("SchId", schedule.ScheduleId);
+            _dc.AddParameter("SchName", schedule.EventName);
+            _dc.AddParameter("SchDate", schedule.EventDate);
+            _dc.AddParameter("SchDay", schedule.EventDay);
+            _dc.AddParameter("SchTime", schedule.EventTime);
+            _dc.AddParameter("UpdateBy", schedule.UpdatedBy);
+            //_dc.AddParameter("RoleId", schedule.RoleId);
+            _dc.Execute("spSchedule_Update");
+        }
+
+        //To Delete the record on a particular User 
+        public void DeleteSchedule(int id)
+        {
+            _dc.ClearParameters();
+            _dc.AddParameter("SchId", id);
+            _dc.AddParameter("UpdatedBy", 1);
+            _dc.ReturnBool("spSchedule_Delete");
+        }
+
+        public IEnumerable<DDLChurch> GetDDLChurch()
+        {
+            List<DDLChurch> Balobj = new List<DDLChurch>();
+            _dc.ClearParameters();
+            DataTable dataTable = _dc.ReturnDataTable("spchurches_Get");
+
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                DDLChurch church = new DDLChurch();
+                church.ChurchId = Convert.ToInt32(dataRow["ChurchId"]);
+                church.ChurchName = dataRow["ChurchName"].ToString();
+                Balobj.Add(church);
+            }
+            return Balobj;
+        }
+
+
+    }
+}
