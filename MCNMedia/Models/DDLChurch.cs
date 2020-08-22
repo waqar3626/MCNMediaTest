5c5bc92e543301ca0837a1ac86ade3e6106ccc9f
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2020-08-21 23:28:30 -0700

    ddlChurch--Khalid--

---
+++ MCNMedia/Models/DDLChurch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCNMedia_Dev.Models
+{
+    public class DDLChurch
+    {
+       
+        public int ChurchId { get; set; }
+        public string ChurchName { get; set; }
+    }
+}
