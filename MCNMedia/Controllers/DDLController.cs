43e03e083c8343cfc7b83664e2da86ffe29cc51f
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2020-08-21 23:02:54 -0700

    Schedule---Khalid--

---
+++ MCNMedia/Controllers/DDLController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MySqlConnector;
+
+namespace MCNMedia_Dev.Controllers
+{
+    public class DDLController : Controller
+    {
+        public ActionResult Index()
+        {
+            String conn = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+            MySqlConnection mySqlConnection = new MySqlConnection(conn);
+            String query = "select UserId,FirstName from tblusers";
+            MySqlCommand mySqlCommand = new MySqlCommand(query, mySqlConnection);
+            mySqlConnection.Open();
+            MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+            DataSet dataSet = new DataSet();
+            mySqlDataAdapter.Fill(dataSet);
+            ViewBag.roleid = dataSet.Tables[0];
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+            foreach (System.Data.DataRow dr in ViewBag.roleid.Rows)
+            {
+                selectListItems.Add(new SelectListItem { Text = @dr["FirstName"].ToString(), Value = @dr["UserId"].ToString() });
+            }
+            ViewBag.State = selectListItems;
+            mySqlConnection.Close();
+            return View();
+        }
+    }
+}
---
+++ MCNMedia/Controllers/ScheduleController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MCNMedia_Dev.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MCNMedia_Dev.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        scheduleDataAccessLayer scheduleDataAccess = new scheduleDataAccessLayer("a");
+
+
+        [HttpGet]
+        public IActionResult AddSchedule()
+        {
+            LoadDDLChurch();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AdddSchedule()
+        {
+            LoadDDLChurch();
+            return View();
+        }
+
+
+        [HttpPost()]
+        public IActionResult AddSchedule(Schedule sch)
+        {
+            LoadDDLChurch();
+            scheduleDataAccess.AddSchedule(sch);
+            return RedirectToAction("ListSchedule");
+        }
+
+        [HttpGet]
+        public IActionResult ListSchedule()
+        {
+            List<Schedule> sch = scheduleDataAccess.GetAllSchedule().ToList<Schedule>();
+            return View(sch);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind] Schedule schedule)
+        {
+            //if (ModelState.IsValid)
+            //{
+            scheduleDataAccess.UpdateSchedule(schedule);
+            return RedirectToAction("ListSchedule");
+            //}
+            //return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult EditSchedule(int id)
+        {
+            Schedule schedule = scheduleDataAccess.GetScheduleDataBtId(id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+            LoadDDLChurch();
+            return View(schedule);
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateSchedule(int id, [Bind] Schedule schedule)
+        {
+
+            scheduleDataAccess.UpdateSchedule(schedule);
+            return RedirectToAction("ListUser");
+          
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            scheduleDataAccess.DeleteSchedule(id);
+            return RedirectToAction("ListSchedule");
+        }
+
+        public void LoadDDLChurch()
+        {
+            IEnumerable<DDLChurch> churches = scheduleDataAccess.GetDDLChurch();
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+            foreach (var item in churches)
+            {
+                selectListItems.Add(new SelectListItem { Text = item.ChurchName.ToString(), Value = item.ChurchId.ToString() });
+            }
+            ViewBag.State = selectListItems;
+
+        }
+    }
+}
