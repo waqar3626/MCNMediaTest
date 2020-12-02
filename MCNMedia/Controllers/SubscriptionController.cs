cc6bde61f1ec721cf6221240c7249282c428f58b
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2020-12-02 17:21:32 +0500

    Subscription Complete Work

--- MCNMedia/Controllers/SubscriptionController.cs
+++ MCNMedia/Controllers/SubscriptionController.cs
@@ -1,79 +1,236 @@
 ﻿using System;
+using MCNMedia_Dev.Models;
+using MCNMedia_Dev.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Configuration;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Amazon.S3.Model.Internal.MarshallTransformations;
-using MCNMedia_Dev.Models;
-using Microsoft.AspNetCore.Mvc;
 using Stripe;
-using MCNMedia_Dev.Repository;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MCNMedia_Dev.Controllers
 {
-    
-      public class SubscriptionController : Controller
-      {
-        PaymentDataAccessLayer paymentDAL = new PaymentDataAccessLayer();
-        private int amount = 500;
-       
+
+    public class SubscriptionController : Controller
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        PlaceAccessLayer _placeAccessLayer = new PlaceAccessLayer();
+        ChurchDataAccessLayer _churchDataAccessLayer = new ChurchDataAccessLayer();
+        WebsiteDataAccessLayer _websiteDataAccessLayer = new WebsiteDataAccessLayer();
+        SubscriptionDataAccessLayer subDataAccess = new SubscriptionDataAccessLayer();
+        ScheduleDataAccessLayer _scheduleDataAccessLayer = new ScheduleDataAccessLayer();
+
+
         public ViewResult Subscribe() {
 
-            ViewBag.PaymentAmount = amount;
+           
             return View();
         }
 
         [HttpPost]
-        public IActionResult Processing(string stripeToken, string stripeEmail)
+        public IActionResult Processing(string Description,string stripeToken, string stripeEmail,decimal PackageAmount)
         {
-            //Add Online Payment Log
-            StripePayment sPayment = new StripePayment();
-            sPayment.SubscriberId = 1;
-            sPayment.OrderId = stripeToken; // "1" + "-" + DateTime.Now.ToString("yyyyMMddHHmmss");
-            sPayment.Amount = amount;
-            sPayment.AmountInCents = (amount / 100);
-            sPayment.OrderDate = DateTime.Now;
-            sPayment.IsSuccess = false;
-            paymentDAL.AddOnlinePaymentLog(sPayment);
-
-
+            Subscriptions subscription = new Subscriptions();
             Dictionary<string, string> Metadata = new Dictionary<string, string>();
             Metadata.Add("Product", "RubberDuck");
             Metadata.Add("Quantity", "10");
             StripeConfiguration.ApiKey = "sk_test_51HsMgeBcBMLkoLiDqppjmP6GfFXnDOjuwsEFPvi2SM0WJZp3yh9zlQbsVDv3lcyUv2FA8aUDuDqJP8bJ6hUwNf1L00RC9aOiHS";
             var options = new ChargeCreateOptions
             {
-                Amount = amount,
+                Amount = (long)PackageAmount*100,
                 Currency = "EUR",
-                Description = "Package Description",
+                Description = Description,
                 Source = stripeToken,
                 ReceiptEmail = stripeEmail,
                 Metadata = Metadata
             };
             var service = new ChargeService();
             Charge charge = service.Create(options);
-            switch (charge.Status)
+           switch (charge.Status)
             {
                 case "succeeded":
-                    //Update OnlinePaymentLog, Add PaymentLog
-                    UpdateOnlinePaymentLog(charge.Status, true, 1, stripeToken);
+                    subscription.ChurchId = (int)HttpContext.Session.GetInt32("chrId");
+                    subscription.SubscriberId=(int)HttpContext.Session.GetInt32("SubscriberId");
+                    subscription.OrderId = stripeToken;
+                    subscription.PackageId= (int)HttpContext.Session.GetInt32("packageId");
+                    subscription.OrderAmount = (decimal)PackageAmount;
+                    subscription.PaidAmount = (decimal)PackageAmount;
+                    int paymentLogId = subDataAccess.AddSubscriberpaymentLog(subscription);
+                    int paymentId = subDataAccess.AddSubscriberpayment(subscription);
+                    return RedirectToAction(nameof(Profile));
                     break;
                 case "failed":
-                    UpdateOnlinePaymentLog(charge.Status, false, 1, stripeToken);
+                    int paymentLogId2 = subDataAccess.AddSubscriberpaymentLog(subscription);
+
                     break;
             }
             return RedirectToAction(nameof(Subscribe));
         }
-             
-        public void UpdateOnlinePaymentLog(string sMessage,bool status,int subscriberId,string orderId)
-        {
-            StripePayment sPayment = new StripePayment();
-            
-            sPayment.SubscriberId = subscriberId;
-            sPayment.OrderId = orderId; // "1" + "-" + DateTime.Now.ToString("yyyyMMddHHmmss");
-            sPayment.IsSuccess = status;
-            paymentDAL.UpdateOnlinePaymentLog(sPayment);
+
+        public IActionResult Packages() {
+
+            List<Subscriptions> subscription = subDataAccess.GetAllSubscription().ToList();
+            return View(subscription);
         }
+
+
+        public IActionResult SubscriptionUserRegistration(int subscriptionId) 
+        {
+            LoadCountryDDL();
+            ViewBag.ErrorMsg = 1;
+            HttpContext.Session.SetInt32("packageId", subscriptionId);
+            
+            return View();
+        }
+
+     [HttpPost]
+        public IActionResult UserRegistration(Subscriptions subscriptions)
+        {
+            int SubscriberId = subDataAccess.AddSubscriber(subscriptions);
+            HttpContext.Session.SetInt32("SubscriberId", SubscriberId);
+            if (SubscriberId > 0) { 
+            int PackageId = (int)HttpContext.Session.GetInt32("packageId");
+            Subscriptions subscription = subDataAccess.GetpackagesById(PackageId);
+            return View("Subscribe", subscription);
+            }
+            else
+            {
+                LoadCountryDDL();
+                ViewBag.ErrorMsg = 2;
+              return View("SubscriptionUserRegistration",subscriptions);
+
+            }
+        }
+
+
+        public IActionResult SubscriptionUserLogin(int subscriptionId)
+        {
+            
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UserLogin(string Email, string Password) 
+        {
+
+            Subscriptions subscription = subDataAccess.SubscriberLogin(Email, Password);
+            if (subscription.SubscriberId>0) {
+                int chrId = (int)HttpContext.Session.GetInt32("chrId");
+                int subscriberId= (int)subscription.SubscriberId;
+                Subscriptions sub = subDataAccess.SubscriberCheck(chrId, subscriberId);
+                if (sub.PaymentId > 0)
+                {
+                    return RedirectToAction(nameof(Profile));
+                }
+                else
+                {
+                    ViewBag.Error = 3;
+                    return View("SubscriptionUserLogin");
+                }
+                            
+            }
+            else
+            {
+                ViewBag.Error = 2;
+                return View("SubscriptionUserLogin");
+            }
+
+        
+        }
+        public void LoadCountryDDL()
+        {
+            try
+            {
+
+                IEnumerable<Place> countryList = _placeAccessLayer.GetCountries();
+                List<SelectListItem> selectListItems = new List<SelectListItem>();
+                foreach (var item in countryList)
+                {
+                    selectListItems.Add(new SelectListItem { Text = item.PlaceName.ToString(), Value = item.PlaceId.ToString() });
+                }
+                ViewBag.Countries = selectListItems;
+
+            }
+            catch (Exception e)
+            {
+              
+                throw;
+            }
+
+
+                    }
+
+
+        public IActionResult Profile()
+        {
+            int id = (int)HttpContext.Session.GetInt32("chrId");
+            ChurchDataAccessLayer churchDataAccess = new ChurchDataAccessLayer();
+            AnnouncementDataAccessLayer announcementDataAccessLayer = new AnnouncementDataAccessLayer();
+            CameraDataAccessLayer camDataAccess = new CameraDataAccessLayer();
+            RecordingDataAccessLayer recordDataAccess = new RecordingDataAccessLayer();
+            ScheduleDataAccessLayer scheduleDataAccess = new ScheduleDataAccessLayer();
+            MediaChurchDataAccessLayer mediaChurchDataAccess = new MediaChurchDataAccessLayer();
+            NoticeDataAccessLayer noticeDataAccess = new NoticeDataAccessLayer();
+            //PreviewChurchesDataAccessLayer previewChurchesDataAccessLayer = new PreviewChurchesDataAccessLayer();
+            ChurchNewsLetterDataAccessLayer churchNewsLetterDataAccess = new ChurchNewsLetterDataAccessLayer();
+
+
+            Profile profileModel = new Profile();
+            string churchSlug = id.ToString();
+            profileModel.Churches = churchDataAccess.GetChurchData(Convert.ToInt32(id));
+            List<Announcement> announcementList = announcementDataAccessLayer.GetAnnouncement(id).ToList();
+            if (announcementList.Count > 0)
+                profileModel.Announcement = announcementList.First<Announcement>();
+            else
+                profileModel.Announcement = new Announcement();
+
+            profileModel.NoticeList = noticeDataAccess.GetAllNotices(id).ToList();
+
+            profileModel.CameraList = camDataAccess.GetAllCameras(id, "");
+            profileModel.VideoList = mediaChurchDataAccess.GetByMediaType("Video", id).ToList();
+            profileModel.SlideshowList = mediaChurchDataAccess.GetByMediaType("SlideShow", id).ToList();
+            profileModel.PictureList = mediaChurchDataAccess.GetByMediaType("Picture", id).ToList();
+            profileModel.newsletter = churchNewsLetterDataAccess.GetLetestNewsletterByChurch(id);
+
+            //profileModel.Cameras = camDataAccess.GetCameraById(1,"");
+            //  profileModel.Media = "";
+            profileModel.RecordingList = recordDataAccess.Recording_GetByChurch(id);
+            profileModel.ScheduleList = scheduleDataAccess.GetSearchSchedule(id, DateTime.Now, DateTime.Now.ToString("dddd"), -1).ToList<Schedule>();
+
+            profileModel.NowScheduleList = Schedules_WhatsOnNow();
+
+            profileModel.ScheduleListDay0 = scheduleDataAccess.GetSearchSchedule(id, System.DateTime.Now, System.DateTime.Now.ToString("dddd"), -1);
+            profileModel.ScheduleListDay1 = scheduleDataAccess.GetSearchSchedule(id, System.DateTime.Now.AddDays(1), System.DateTime.Now.AddDays(1).ToString("dddd"), -1);
+            profileModel.ScheduleListDay2 = scheduleDataAccess.GetSearchSchedule(id, System.DateTime.Now.AddDays(2), System.DateTime.Now.AddDays(2).ToString("dddd"), -1);
+            profileModel.ScheduleListDay3 = scheduleDataAccess.GetSearchSchedule(id, System.DateTime.Now.AddDays(3), System.DateTime.Now.AddDays(3).ToString("dddd"), -1);
+            profileModel.ScheduleListDay4 = scheduleDataAccess.GetSearchSchedule(id, System.DateTime.Now.AddDays(4), System.DateTime.Now.AddDays(4).ToString("dddd"), -1);
+            profileModel.ScheduleListDay5 = scheduleDataAccess.GetSearchSchedule(id, System.DateTime.Now.AddDays(5), System.DateTime.Now.AddDays(5).ToString("dddd"), -1);
+            profileModel.ScheduleListDay6 = scheduleDataAccess.GetSearchSchedule(id, System.DateTime.Now.AddDays(6), System.DateTime.Now.AddDays(6).ToString("dddd"), -1);
+
+            return View(profileModel);
+        }
+
+        private IEnumerable<Schedule> Schedules_WhatsOnNow()
+        {
+            try
+            {
+
+                List<Schedule> schedules = _scheduleDataAccessLayer.GetWebsiteSchedule_WhatsOnNow().ToList<Schedule>();
+                return schedules;
+            }
+            catch (Exception e)
+            {
+
+                
+                throw;
+            }
+        }
+
+
     }
 }
---
+++ MCNMedia/Repository/SubscriptionDataAccessLayer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using MCNMedia_Dev.Models;
+
+namespace MCNMedia_Dev.Repository
+{
+    public class SubscriptionDataAccessLayer
+    {
+        AwesomeDal.DatabaseConnect _dc;
+
+        public SubscriptionDataAccessLayer()
+        {
+            _dc = new AwesomeDal.DatabaseConnect();
+        }
+
+        public IEnumerable<Subscriptions> GetAllSubscription()
+        {
+            List<Subscriptions> Balobj = new List<Subscriptions>();
+            DataTable dataTable = GetSubscriptionPackages(PackageId: -1);
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                Subscriptions subscription = BindSubscriptionPackages(dataRow);
+                Balobj.Add(subscription);
+            }
+            return Balobj;
+        }
+
+        public Subscriptions GetpackagesById(int PackageId)
+        {
+            Subscriptions subscription = new Subscriptions();
+            DataTable dataTable = GetSubscriptionPackages(PackageId: PackageId);
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                subscription = BindSubscriptionPackages(dataRow);
+            }
+            return subscription;
+        }
+        public DataTable GetSubscriptionPackages(int PackageId)
+        {
+            List<Subscriptions> Balobj = new List<Subscriptions>();
+            _dc.ClearParameters();
+            _dc.AddParameter("PackageId", PackageId);
+
+            DataTable dataTable = _dc.ReturnDataTable("spPackages_Get");
+            return dataTable;
+               
+        }
+
+            private Subscriptions BindSubscriptionPackages(DataRow dataRow)
+            {
+
+                Subscriptions subscription = new Subscriptions();
+                subscription.PackageId = Convert.ToInt32(dataRow["PackageId"]);
+                subscription.Duration = Convert.ToInt32(dataRow["Duration"]);
+                subscription.PackageTitle = dataRow["PackageTitle"].ToString();
+                subscription.DurationUnit = dataRow["DurationUnit"].ToString();
+                subscription.Currency = dataRow["Currency"].ToString();
+                subscription.PackageCharge = Convert.ToDecimal(dataRow["PackageCharge"].ToString());
+                return subscription;
+        }
+        public int AddSubscriber(Subscriptions Sub)
+        {
+            _dc.ClearParameters();
+            _dc.AddParameter("Subname", Sub.Name);
+            _dc.AddParameter("countryId", Sub.CountryId);
+            _dc.AddParameter("Email", Sub.EmailAddress);
+            _dc.AddParameter("Subpassword", Sub.Password);
+
+            int SubscriberId = _dc.ReturnInt("spSubscribers_Add");
+
+            return SubscriberId;
+        }
+
+        public int AddSubscriberpayment(Subscriptions Sub)
+        {
+            _dc.ClearParameters();
+            _dc.AddParameter("PackageId", Sub.PackageId);
+            _dc.AddParameter("SubscriberId", Sub.SubscriberId);
+            _dc.AddParameter("OrderId", Sub.OrderId);
+            _dc.AddParameter("OrderAmount", Sub.OrderAmount);
+            _dc.AddParameter("PaidAmount", Sub.PaidAmount);
+            _dc.AddParameter("ChurchId", Sub.ChurchId);
+
+
+
+            return _dc.ReturnInt("spSubscriberPayment_Add");
+
+         
+        }
+
+        public int  AddSubscriberpaymentLog(Subscriptions Sub)
+        {
+            _dc.ClearParameters();
+            _dc.AddParameter("PackageId", Sub.PackageId);
+            _dc.AddParameter("SubscriberId", Sub.SubscriberId);
+            _dc.AddParameter("OrderId", Sub.OrderId);
+            _dc.AddParameter("OrderAmount", Sub.OrderAmount);
+            _dc.AddParameter("PaidAmount", Sub.PaidAmount);
+            _dc.AddParameter("ChurchId", Sub.ChurchId);
+
+
+
+            return _dc.ReturnInt("spSubscriberPaymentLog_Add");
+
+            
+        }
+
+        public Subscriptions SubscriberLogin(string EmailAddress, string LoginPassword)
+        {
+            Subscriptions user = new Subscriptions();
+
+            _dc.ClearParameters();
+            _dc.AddParameter("EmailAdd", EmailAddress);
+            _dc.AddParameter("logPassword", LoginPassword);
+
+            DataTable dataTable = _dc.ReturnDataTable("spSubscriber_Login");
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                user.SubscriberId = Convert.ToInt32(dataRow["SubscriberId"]);
+                user.Name = dataRow["SubscriberName"].ToString();
+                user.EmailAddress = dataRow["EmailAddress"].ToString();
+                user.CountryId = Convert.ToInt32(dataRow["CountryId"]);
+                user.Password = dataRow["SubscriberPassword"].ToString();
+               
+            }
+            return user;
+        }
+        public Subscriptions SubscriberCheck(int ChurchId , int SubscriberId)
+        {
+            Subscriptions user = new Subscriptions();
+
+            _dc.ClearParameters();
+            _dc.AddParameter("chrchId", ChurchId);
+            _dc.AddParameter("SubscriberId", SubscriberId);
+
+            DataTable dataTable = _dc.ReturnDataTable("spSubscription_Check");
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                user.PaymentId = Convert.ToInt32(dataRow["PaymentId"]);
+                user.PackageId = Convert.ToInt32(dataRow["PackageId"]);
+                user.SubscriberId = Convert.ToInt32(dataRow["SubscriberId"]);
+                user.ChurchId = Convert.ToInt32(dataRow["ChurchId"]);
+                user.OrderId= dataRow["OrderId"].ToString();
+                user.OrderAmount = Convert.ToDecimal(dataRow["OrderAmount"].ToString());
+                user.PaidAmount = Convert.ToDecimal(dataRow["PaidAmount"].ToString());
+                 }
+            return user;
+        }
+    }
+}
