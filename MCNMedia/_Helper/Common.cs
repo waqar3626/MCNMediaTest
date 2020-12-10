1cd0c526c211bb811f77f7479bbd63b61c2ccfcd
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2020-12-10 15:44:18 +0500

    - logs -- Ishfaq

---
+++ MCNMedia/_Helper/Common.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCNMedia_Dev._Helper
+{
+    public class Common
+    {
+
+        static AwesomeDal.DatabaseConnect _dc;
+
+        public static bool SaveToXXX(string message)
+        {
+            _dc = new AwesomeDal.DatabaseConnect();
+            _dc.ClearParameters();
+            _dc.AddParameter("msg", message);
+            return _dc.ReturnBool("spLog_Add");
+        }
+
+
+
+    }
+}
