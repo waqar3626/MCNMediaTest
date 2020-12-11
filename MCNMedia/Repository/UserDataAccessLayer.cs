436eca8dd7cf576e467ba54675700f9780901a2c
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2020-12-11 16:48:51 +0500

    - Message updated on change password -- Ishfaq

--- MCNMedia/Repository/UserDataAccessLayer.cs
+++ MCNMedia/Repository/UserDataAccessLayer.cs
@@ -1,4 +1,5 @@
-﻿using MCNMedia_Dev.Models;
+﻿using _Helper;
+using MCNMedia_Dev.Models;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
@@ -28,7 +29,7 @@ namespace MCNMedia_Dev.Repository
             _dc.AddParameter("EmailAdd", usr.EmailAddress);
             _dc.AddParameter("Rolech", usr.RoleId);
             DataTable dataTable = _dc.ReturnDataTable("spUser_Churches_Search");
-            foreach(DataRow dataRow in dataTable.Rows)
+            foreach (DataRow dataRow in dataTable.Rows)
             {
                 User user = new User();
                 user.UserId = Convert.ToInt32(dataRow["UserId"]);
@@ -38,16 +39,16 @@ namespace MCNMedia_Dev.Repository
                 user.EmailAddress = dataRow["EmailAddress"].ToString();
                 user.LoginPassword = dataRow["LoginPassword"].ToString();
                 user.RoleName = dataRow["RoleName"].ToString();
-                user.UserName=user.FirstName + " " + user.LastName;
+                user.UserName = user.FirstName + " " + user.LastName;
 
                 Balobj.Add(user);
             }
 
             return Balobj;
         }
-		
-		// get user role
-		public IEnumerable<UserRoles> GetRoles()
+
+        // get user role
+        public IEnumerable<UserRoles> GetRoles()
         {
             List<UserRoles> Balobj = new List<UserRoles>();
             _dc.ClearParameters();
@@ -62,20 +63,21 @@ namespace MCNMedia_Dev.Repository
             }
             return Balobj;
         }
-        
-		//To Add new User record    
+
+        //To Add new User record    
         public void AddUser(User user)
         {
             _dc.ClearParameters();
             _dc.AddParameter("FirstName", user.FirstName);
             _dc.AddParameter("LastName", user.LastName);
             _dc.AddParameter("EmailAddress", user.EmailAddress);
-            _dc.AddParameter("LoginPassword", user.LoginPassword);
+            _dc.AddParameter("LoginPassword", Hashing.HashPassword(user.LoginPassword));
             _dc.AddParameter("UserId", user.UpdatedBy);
             _dc.AddParameter("RoleId", user.RoleId);
             _dc.Execute("spUser_Add");
         }
-		//To Update the records of a particluar User
+
+        //To Update the records of a particluar User
         public void UpdateUser(User user)
         {
             _dc.ClearParameters();
@@ -87,22 +89,17 @@ namespace MCNMedia_Dev.Repository
             _dc.AddParameter("RoleId", user.RoleId);
             _dc.Execute("spUser_Update");
         }
+
         //Get the details of a particular User
         public User GetUserData(int id)
         {
             User user = new User();
-
             _dc.ClearParameters();
             _dc.AddParameter("UsrId", id);
             DataTable dataTable = _dc.ReturnDataTable("spUser_GetbyId");
             foreach (DataRow dataRow in dataTable.Rows)
             {
-                user.UserId = Convert.ToInt32(dataRow["UserId"]);
-                user.FirstName = dataRow["FirstName"].ToString();
-                user.LastName = dataRow["LastName"].ToString();
-                user.EmailAddress = dataRow["EmailAddress"].ToString();
-                user.LoginPassword = dataRow["LoginPassword"].ToString();
-                user.RoleId = Convert.ToInt32(dataRow["RoleId"]);
+                user = BindUserData(dataRow);
             }
             return user;
         }
@@ -110,7 +107,7 @@ namespace MCNMedia_Dev.Repository
         public User UserLogin(User usr)
         {
             User user = new User();
-
+            Status sts = new Status();
             _dc.ClearParameters();
             _dc.AddParameter("EmailAdd", usr.EmailAddress);
             _dc.AddParameter("logPassword", usr.LoginPassword);
@@ -118,30 +115,40 @@ namespace MCNMedia_Dev.Repository
             DataTable dataTable = _dc.ReturnDataTable("spUser_Login");
             foreach (DataRow dataRow in dataTable.Rows)
             {
-                user.UserId = Convert.ToInt32(dataRow["UserId"]);
-                user.FirstName = dataRow["FirstName"].ToString();
-                user.LastName = dataRow["LastName"].ToString();
-                user.EmailAddress = dataRow["EmailAddress"].ToString();
-                user.LoginPassword = dataRow["LoginPassword"].ToString();
-                user.RoleId = Convert.ToInt32(dataRow["RoleId"]);
-                user.RoleName = dataRow["RoleName"].ToString();
+                sts = Hashing.ValidatePassword(usr.LoginPassword, dataRow["LoginPassword"].ToString());
+                if (sts.Success)
+                {
+                    user = BindUserData(dataRow);
+                }
             }
             return user;
         }
-        
-		//To Delete the record on a particular User 
-		public void DeleteUser(int id,int UpdateBy)
+
+        private User BindUserData(DataRow dataRow)
+        {
+            User user = new User();
+            user.UserId = Convert.ToInt32(dataRow["UserId"]);
+            user.FirstName = dataRow["FirstName"].ToString();
+            user.LastName = dataRow["LastName"].ToString();
+            user.EmailAddress = dataRow["EmailAddress"].ToString();
+            user.LoginPassword = dataRow["LoginPassword"].ToString();
+            user.RoleId = Convert.ToInt32(dataRow["RoleId"]);
+            user.RoleName = dataRow["RoleName"].ToString();
+            return user;
+        }
+
+        //To Delete the record on a particular User 
+        public void DeleteUser(int id, int UpdateBy)
         {
             _dc.ClearParameters();
             _dc.AddParameter("UsrId", id);
             _dc.AddParameter("UpdateBy", UpdateBy);
             _dc.ReturnBool("spUser_Delete");
         }
-        public void ChangeUserPassword(int UserId ,string Password , int UpdateBy)
+
+        public void ChangeUserPassword(int UserId, string Password, int UpdateBy)
         {
             _dc.ClearParameters();
-            
-            
             _dc.AddParameter("UsrId", UserId);
             _dc.AddParameter("LoginPass", Password);
             _dc.AddParameter("UpdateBy", UpdateBy);
