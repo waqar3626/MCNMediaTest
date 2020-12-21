415363131a8f5b50cf2a96b4c1a3edda6f5e905e
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2020-12-21 10:39:07 +0500

    model church Donation--khalid

---
+++ MCNMedia/Models/ChurchDonation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCNMedia_Dev.Models
+{
+    public class ChurchDonation
+    {
+        public int DoonationId { get; set; }
+        public int ChurchId { get; set; }
+        public String ImageUrl { get; set; }
+        public String WebSiteUrl { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+        public Boolean mDelete { get; set; }
+    }
+}
