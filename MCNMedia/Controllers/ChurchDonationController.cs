b7673310a7b82c594e21c1e3ffb0b89a78e18199
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2020-12-21 10:24:50 +0500

    churchDonation--Khalid

---
+++ MCNMedia/Controllers/ChurchDonationController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MCNMedia_Dev._Helper;
+using MCNMedia_Dev.Models;
+using MCNMedia_Dev.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCNMedia_Dev.Controllers
+{
+
+    public class ChurchDonationController : Controller
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        ChurchDonationDataAccessLayer DonationDataAccessLayer = new ChurchDonationDataAccessLayer();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListDonation()
+        {
+            ChurchDonation donation = new ChurchDonation();
+            donation.ChurchId = (int)HttpContext.Session.GetInt32("ChurchId");
+            List<ChurchDonation> churchDonations = DonationDataAccessLayer.GetDonationById(donation).ToList();
+            return View(churchDonations);
+        }
+
+
+        [HttpPost]
+        public JsonResult AddDonation(string WebsiteUrl, IFormFile mediaFile)
+        {
+            try
+            {
+                ChurchDonation churchDonation = new ChurchDonation();
+                if (!string.IsNullOrEmpty(HttpContext.Session.GetInt32("ChurchId").ToString()))
+                {
+                    churchDonation.ChurchId = (int)HttpContext.Session.GetInt32("ChurchId");
+                    churchDonation.UpdatedBy = (int)HttpContext.Session.GetInt32("UserId");
+                    churchDonation.WebSiteUrl = WebsiteUrl;
+                    churchDonation.ImageUrl = FileUploadUtility.UploadFile(mediaFile, UploadingAreas.Picture, churchDonation.ChurchId);
+
+                    int res = DonationDataAccessLayer.AddDonation(churchDonation);
+                }
+                return Json(1);
+            }
+            catch (Exception e)
+            {
+                ShowMessage("Add Donation Error" + e.Message);
+                throw;
+            }
+
+        }
+        public IActionResult EditDonation(int id)
+        {
+            try
+            {
+                ChurchDonation donation = new ChurchDonation();
+                donation.ChurchId=(int)HttpContext.Session.GetInt32("ChurchId");
+                //donation = DonationDataAccessLayer.GetDonationById(donation.ChurchId);
+                return PartialView("EditPicture", donation);
+            }
+            catch (Exception e)
+            {
+                ShowMessage("Edit Donation Error" + e.Message);
+                throw;
+            }
+
+        }
+
+        public IActionResult DeleteDonation(int id)
+        {
+            try
+            {
+                ChurchDonation donation = new ChurchDonation();
+                int UpdateBy = (int)HttpContext.Session.GetInt32("UserId");
+                bool res = DonationDataAccessLayer.DeleteDonation(id, UpdateBy);
+                return Json(res);
+            }
+            catch (Exception e)
+            {
+                ShowMessage("Delete Donation Error" + e.Message);
+                throw;
+            }
+
+
+        }
+
+
+        private void ShowMessage(string exceptionMessage)
+        {
+            log.Info("Exception: " + exceptionMessage);
+        }
+    }
+}
---
+++ MCNMedia/Repository/ChurchDonationDataAccessLayer.cs
@@ -0,0 +1,79 @@
+﻿using MCNMedia_Dev.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCNMedia_Dev.Repository
+{
+    public class ChurchDonationDataAccessLayer
+    {
+        AwesomeDal.DatabaseConnect _dc;
+
+        private readonly string AWS_S3_BUCKET_URI;
+
+        public ChurchDonationDataAccessLayer()
+        {
+            _dc = new AwesomeDal.DatabaseConnect();
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"));
+            var root = builder.Build();
+            var awsS3bucket = root.GetSection("S3BucketConfiguration");
+            var sysConfig = root.GetSection("SystemConfiguration");
+            AWS_S3_BUCKET_URI = $"{awsS3bucket["aws_bucket_url"]}/{sysConfig["system_mode"]}";
+        }
+
+        public int AddDonation(ChurchDonation donation)
+        {
+            _dc.ClearParameters();
+            _dc.AddParameter("UserId", donation.UpdatedBy);
+            _dc.AddParameter("WebsiteUrl", donation.WebSiteUrl);
+            _dc.AddParameter("ImageUrl", donation.ImageUrl);
+            _dc.AddParameter("ChurchId", donation.ChurchId);
+            return _dc.Execute("spChurchDonation_Insert");
+        }
+
+        public IEnumerable<ChurchDonation> GetDonationById(ChurchDonation chrdonation)
+        {
+            List<ChurchDonation> donation = new List<ChurchDonation>();
+
+            _dc.ClearParameters();
+            _dc.AddParameter("Church_Id", chrdonation.ChurchId);
+            DataTable dataTable = _dc.ReturnDataTable("spChurchDonation_GetByChurchId");
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                ChurchDonation churchDonation = new ChurchDonation();
+                churchDonation.DoonationId = Convert.ToInt32(dataRow["DonationId"]);
+                churchDonation.ImageUrl = $"{AWS_S3_BUCKET_URI}/{dataRow["ImageUrl"]}";
+                churchDonation.WebSiteUrl = dataRow["WebsiteUrl"].ToString();
+                donation.Add(churchDonation);
+            }
+            return donation;
+        }
+
+        public int UpdateDonation(MediaChurch med)
+        {
+            _dc.ClearParameters();
+            _dc.AddParameter("MediaId", med.ChurchMediaId);
+            _dc.AddParameter("MedTabName", med.TabName);
+            _dc.AddParameter("MedURL", med.MediaURL);
+            _dc.AddParameter("MedName", med.MediaName);
+            _dc.AddParameter("UserId", med.UpdatedBy);
+
+            return _dc.Execute("spChurchMedia_Update");
+        }
+
+
+        public bool DeleteDonation(int DonationId, int UpdateBy)
+        {
+            _dc.ClearParameters();
+            _dc.AddParameter("DonationId", DonationId);
+            _dc.AddParameter("UserId", UpdateBy);
+            return _dc.ReturnBool("spChurchMedia_Delete");
+        }
+
+    }
+}
