c8f00c0b8c44ab68fe11d7380cda4180fae4b05d
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2021-01-08 09:34:22 +0500

    RssFeed Model Class --Uxair--

---
+++ MCNMedia/Models/RssFeedModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCNMedia_Dev.Models
+{
+    public class RssFeedModel
+    {
+        public int RssFeedId { get; set; }
+
+        public string RssFeedTitle { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime PublishDate { get; set; }
+    }
+}
