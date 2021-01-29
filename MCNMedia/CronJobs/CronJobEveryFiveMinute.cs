85e400af1fd8a944c044cd1464a7c99c3f59a037
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2021-01-29 18:00:22 +0500

    - Start/Stop recording sequence updated - Video cover image updated -- Ishfaq

--- MCNMedia/CronJobs/CronJobEveryFiveMinute.cs
+++ MCNMedia/CronJobs/CronJobEveryFiveMinute.cs
@@ -8,10 +8,10 @@ namespace MCNMedia_Dev.CronJobs
 {
     public class CronJobEveryFiveMinute : CronJobService
     {
-        private readonly ILogger<CronJobEveryMinute> _logger;
+        private readonly ILogger<CronJobEveryFiveMinute> _logger;
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        public CronJobEveryFiveMinute(IScheduleConfig<CronJobEveryMinute> config, ILogger<CronJobEveryMinute> logger)
+        public CronJobEveryFiveMinute(IScheduleConfig<CronJobEveryFiveMinute> config, ILogger<CronJobEveryFiveMinute> logger)
             : base(config.CronExpression, config.TimeZoneInfo)
         {
             _logger = logger;
@@ -19,49 +19,43 @@ namespace MCNMedia_Dev.CronJobs
 
         public override Task StartAsync(CancellationToken cancellationToken)
         {
-            log.Info("CronJob Every 5 Minute starts.");
+            log.Info("JOB - CronJob Every 5 Minute starts.");
             return base.StartAsync(cancellationToken);
         }
 
         public override Task DoWork(CancellationToken cancellationToken)
         {
-            log.Info($"{DateTime.Now:hh:mm:ss} CronJob Every Minute is working.");
+            log.Info($"{DateTime.Now:hh:mm:ss} CronJob Every 5 is working.");
             ScheduleController schedule = new ScheduleController();
             try
             {
-                schedule.StartRecording();
-            }
-            catch(Exception ex)
-            {
-                log.Info("CronJob Every 5 Minute - Start Recording - Error - " + ex.Message.ToString());
-                _Helper.Common.SaveToXXX("CronJob Every 5 Minute - Start Recording - Error - " + ex.Message.ToString());
-            }
-            try
-            {
+                _Helper.Common.SaveToXXX("JOB - StopRecording - Begin");
                 schedule.StopRecording();
+                _Helper.Common.SaveToXXX("JOB - StopRecording - End");
             }
             catch (Exception ex)
             {
                 log.Info("CronJob Every 5 Minute - Stop Recording - Error - " + ex.Message.ToString());
                 _Helper.Common.SaveToXXX("CronJob Every 5 Minute - Stop Recording - Error - " + ex.Message.ToString());
             }
-            
             try
             {
-                CameraController camera = new CameraController();
-                camera.SyncAllCamerasWithWowza();
+                _Helper.Common.SaveToXXX("JOB - StartRecording - Begin");
+                schedule.StartRecording();
+                _Helper.Common.SaveToXXX("JOB - StartRecording - End");
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
-                log.Info("CronJob Every 5 Minute - Sync All Cam - Error - " + ex.Message.ToString());
-                _Helper.Common.SaveToXXX("CronJob Every 5 Minute - Sync All Cam - Error - " + ex.Message.ToString());
+                log.Info("CronJob Every 5 Minute - Start Recording - Error - " + ex.Message.ToString());
+                _Helper.Common.SaveToXXX("CronJob Every 5 Minute - Start Recording - Error - " + ex.Message.ToString());
             }
+            
             return Task.CompletedTask;
         }
 
         public override Task StopAsync(CancellationToken cancellationToken)
         {
-            log.Info("CronJob Every 5 Minute is stopping.");
+            log.Info("JOB - CronJob Every 5 Minute is stopping.");
             return base.StopAsync(cancellationToken);
         }
     }
