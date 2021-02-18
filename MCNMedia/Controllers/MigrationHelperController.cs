225294aafa28a799ee1c502a8b74727b40520146
ishfaqahmad185 <ishfaqahmad185@gmail.com>
2021-02-18 17:32:15 +0500

    - Migration methods -- Ishfaq

--- MCNMedia/Controllers/MigrationHelperController.cs
+++ MCNMedia/Controllers/MigrationHelperController.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Transfer;
+using Google.Apis.Analytics.v3;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
 using MCNMedia_Dev.Models;
 using MCNMedia_Dev.Repository;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace MCNMedia_Dev.Controllers
 {
@@ -31,18 +42,17 @@ namespace MCNMedia_Dev.Controllers
             int counter = 0;
             foreach (Camera cam in cameraList)
             {
-                if(cam.IsCameraStreaming == false)
-                {
-                    wowzaHelper.RequestCameraStatus(cam.ChurchId, cam.CameraId);
-                }
+                //if (cam.IsCameraStreaming == false)
+                //{
+                //    wowzaHelper.RequestCameraStatus(cam.ChurchId, cam.CameraId);
+                //}
                 counter++;
                 if (counter <= 1000)
                 {
                     wowzaHelper = new WowzaApi.WowzaHelper();
                     wowzaHelper.RequestCamera(cam.ChurchId, cam.CameraId, cam.CameraUrl);
-                    Thread.Sleep(1000);
+                    Thread.Sleep(100);
                 }
-
             }
         }
 
@@ -134,8 +144,154 @@ namespace MCNMedia_Dev.Controllers
                     }
                 }
             }
-                       
+
         }
 
+        public void RequestCameraStatus()
+        {
+
+            RequestCameraStatusa("52.51.59.126");
+
+            RequestCameraStatusa("52.211.229.30");
+
+        }
+
+        private DataTable RequestCameraStatusa(string ip)
+        {
+            string error = "";
+            DataTable dt = GetTable();
+            StreamFiles streamFiles = new StreamFiles();
+            StreamFile cameraStream = new StreamFile();
+            try
+            {
+                string requestUrl = $"http://{ip}:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/live/streamfiles";
+                HttpClient client = CreateHttpClientRequest(requestUrl);
+                // List data response.
+                HttpResponseMessage response = client.GetAsync("").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = response.Content.ReadAsStringAsync().Result;
+                    try
+                    {
+                        streamFiles = JsonConvert.DeserializeObject<StreamFiles>(responseBody);
+
+                        foreach (var streamFile in streamFiles.streamFiles)
+                        {
+
+                            HttpClient clienta = CreateHttpClientRequest($"{requestUrl}/{streamFile.id}");
+                            // List data response.
+                            HttpResponseMessage responsea = clienta.GetAsync("").Result;
+                            if (responsea.IsSuccessStatusCode)
+                            {
+                                var responseBodya = responsea.Content.ReadAsStringAsync().Result;
+                                try
+                                {
+                                    cameraStream = JsonConvert.DeserializeObject<StreamFile>(responseBodya);
+                                    int cameraId = 0;
+                                    if (streamFile.id.Contains("_"))
+                                    {
+                                        string camId = streamFile.id.Split("_").Last();
+                                        if (Microsoft.VisualBasic.Information.IsNumeric(camId))
+                                        {
+                                            cameraId = Convert.ToInt32(camId);
+                                        }
+                                    }
+                                    dt.Rows.Add(cameraId, cameraStream.serverName, cameraStream.name, cameraStream.uri, ip, streamFile.id);
+                                }
+                                catch (Exception ex)
+                                {
+                                    error += ex.Message;
+                                    _Helper.Common.SaveToXXX(ex.Message);
+                                }
+                            }
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        error += ex.Message;
+                        _Helper.Common.SaveToXXX(ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                error += ex.Message;
+                _Helper.Common.SaveToXXX(ex.Message);
+            }
+            dt.Rows.Add(-1, "", "", "","", error);
+            return dt;
+        }
+
+        public static bool IsNumeric(object Expression)
+        {
+            double retNum;
+
+            bool isNum = Double.TryParse(Convert.ToString(Expression), System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+            return isNum;
+        }
+        public DataTable GetTable()
+        {
+            DataTable table = new DataTable();
+
+            //' Create four typed columns in the DataTable.
+            table.Columns.Add("CameraId", typeof(int));
+            table.Columns.Add("serverName", typeof(string));
+            table.Columns.Add("name", typeof(string));
+            table.Columns.Add("uri", typeof(string));
+            table.Columns.Add("version", typeof(string));
+            table.Columns.Add("streamName", typeof(string));
+            return table;
+        }
+
+
+        private HttpClient CreateHttpClientRequest(string requestUri)
+        {
+            Uri uri = new Uri(requestUri);
+            var HttpHandler = new HttpClientHandler();
+            HttpHandler.Credentials = GetCredentials(uri);
+
+            HttpClient client = new HttpClient(HttpHandler);
+            client.BaseAddress = new Uri(requestUri);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+
+        private CredentialCache GetCredentials(Uri uri)
+        {
+            var credentialCache = new CredentialCache();
+            credentialCache.Add(
+            new Uri(uri.GetLeftPart(UriPartial.Authority)), // request url's host
+            "Digest", // authentication type 
+            new NetworkCredential("mcnmedia", "ykdEHGc6XH6e35") // credentials 
+            );
+            return credentialCache;
+        }
+    }
+
+    public class StreamFiles
+    {
+        public string serverName { get; set; }
+        public List<StreamFileInfo> streamFiles { get; set; }
+    }
+
+    [DataContract]
+    public class StreamFileInfo
+    {
+        [DataMember]
+        public string id { get; set; }
+
+        [DataMember]
+        public string href { get; set; }
+    }
+    public class StreamFile
+    {
+        public string version { get; set; }
+        public string serverName { get; set; }
+        public string name { get; set; }
+        public string uri { get; set; }
     }
 }
